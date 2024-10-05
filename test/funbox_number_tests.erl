3b884dcaf05001d1e13c42f086a64f92d2504e3b
Dmitri Vereshchagin <dmitri.vereshchagin@gmail.com>
2024-10-05 23:39:22 +0300

    Replace commented assert macros with EUnit titles
    While it appears that any term can be used as assertion comment,
    the documentation states that it must be character data.  So there is
    no reason to use commented assert macros instead of EUnit titles.

--- test/funbox_number_tests.erl
+++ test/funbox_number_tests.erl
@@ -12,19 +12,19 @@ from_binary_test_() ->
      ?_assertEqual(error, funbox_number:from_binary(<<"x">>))].
 
 is_prime_test_() ->
-    [?_test(?assertEqual(R, funbox_number:is_prime(N), T))
-     || {N, R} = T <- [{-1, false},
-                       {0, false},
-                       {1, false},
-                       {2, true},
-                       {3, true},
-                       {2 * 2, false},
-                       {2 * 3, false},
-                       {3 * 3, false}]].
+    [{integer_to_list(N), ?_assertEqual(R, funbox_number:is_prime(N))}
+     || {N, R} <- [{-1, false},
+                   {0, false},
+                   {1, false},
+                   {2, true},
+                   {3, true},
+                   {2 * 2, false},
+                   {2 * 3, false},
+                   {3 * 3, false}]].
 
 primes_test_() ->
     %% https://en.wikipedia.org/wiki/List_of_prime_numbers
-    [?_test(?assert(funbox_number:is_prime(N), {number, N}))
+    [{integer_to_list(N), ?_assert(funbox_number:is_prime(N))}
      || N <- [31636373, 39916801, 193877777, 433494437, 479001599,
               2971215073, 8589935681, 8589935681, 16148168401,
               16148168401, 22815088913, 77777677777, 87178291199,
@@ -32,7 +32,7 @@ primes_test_() ->
 
 pseudoprimes_test_() ->
     %% https://en.wikipedia.org/wiki/Pseudoprime
-    [?_test(?assertNot(funbox_number:is_prime(N), {number, N}))
+    [{integer_to_list(N), ?_assertNot(funbox_number:is_prime(N))}
      || N <- [7957, 8321, 8481, 8911, 272611, 283361, 302101, 303101,
               12327121, 443372888629441]].
 
