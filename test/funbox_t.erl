fc7d2aa707940b616a5a0d8cc82a318aae7a8506
Dmitri Vereshchagin <dmitri.vereshchagin@gmail.com>
2024-12-15 14:36:37 +0300

    Refactor funbox_t

--- test/funbox_t.erl
+++ test/funbox_t.erl
@@ -4,30 +4,32 @@
 -export([wait_until/2]).
 
 %%%===================================================================
-%%% Macro definitions
+%%% Types
 %%%===================================================================
 
--define(NOW(), erlang:monotonic_time(millisecond)).
+-type wait_until_option() :: {timeout, timeout()} |
+                             {delay, non_neg_integer()}.
+-export_type([wait_until_option/0]).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
--spec wait_until(fun(() -> boolean()), [Option]) ->
-          ok | timeout when
-      Option :: {timeout, timeout()} |
-                {delay, non_neg_integer()}.
+-spec wait_until(Pred, Opts) -> ok | timeout when
+      Pred :: fun(() -> boolean()),
+      Opts :: [wait_until_option()].
 wait_until(Pred, Opts) ->
-    Time  = proplists:get_value(timeout, Opts, 5000),
+    Timeout = proplists:get_value(timeout, Opts, 5000),
+    Deadline = monotonic_time() + Timeout,
     Delay = proplists:get_value(delay, Opts, 100),
-    wait_until(Pred, ?NOW() + Time, Delay).
+    wait_until(Pred, Deadline, Delay).
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
 wait_until(Pred, Deadline, Delay) ->
-    case {Pred(), ?NOW()} of
+    case {Pred(), monotonic_time()} of
         {true, _Now} ->
             ok;
         {false, Now} when Now < Deadline ->
@@ -36,3 +38,6 @@ wait_until(Pred, Deadline, Delay) ->
         {false, _Now} ->
             timeout
     end.
+
+monotonic_time() ->
+    erlang:monotonic_time(millisecond).
